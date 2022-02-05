a098dd402c4df022ba7374486f46d1fbf4faca4e
Cameron Sanders <csanders@protonmail.com>
2022-02-04 21:02:14 -0600

    more cleanup

--- config/systems/nixos-homeserver/modules.nix
+++ config/systems/nixos-homeserver/modules.nix
@@ -1,12 +1,9 @@
-{ config, pkgs, ... }:
-let cfg = config.cfg;
-in 
-{
+{ config, pkgs, ... }: {
   imports = [
     ../../global/global.nix
+    ../../modules/common/fonts/fonts.nix
     ../../modules/common/shell/shell.nix
     ../../modules/common/user-main/user-main.nix
-    ../../modules/common/fonts/fonts.nix
     ../../modules/common/utils/utils.nix
     ../../modules/nixos/__nixos/__nixos.nix
     ../../modules/nixos/desktop-environment-gnome/desktop-environment-gnome.nix
@@ -15,9 +12,12 @@ in
     ../../modules/nixos/service-cron/service-cron.nix
     ../../modules/nixos/service-docker/service-docker.nix
     ../../modules/nixos/service-firewall/service-firewall.nix
+    ../../modules/nixos/service-networking/service-networking.nix
     ../../modules/nixos/service-samba/service-samba.nix
     ../../modules/nixos/service-ssh/service-ssh.nix
     ../../modules/nixos/service-sudo/service-sudo.nix
+    ../../modules/nixos/service-virtualbox/service-virtualbox.nix
+    ../../modules/nixos/service-xrdp/service-xrdp.nix
     ./modules.users.nix
     ./modules/cron.nix
     ./modules/docker.nix
