2249c4c63b05dcc0682c235ad71dabc490dad5d7
Cameron Sanders <csanders@protonmail.com>
2022-02-06 20:00:11 -0600

    docker config

--- config/systems/nixos-homeserver/modules/docker.nix
+++ config/systems/nixos-homeserver/modules/docker.nix
@@ -1,33 +1,37 @@
 { config, lib, pkgs, options, ... }: {
-  config.docker-containers = {
-    # portainer = {
-    #   image = "portainer/portainer";
-    #   ports = [ "8000:8000" "9000:9000" ];
-    #   volumes = [
-    #     "/var/run/docker.sock:/var/run/docker.sock"
-    #     "portainer_data:/data"
-    #   ];
-    #   environment = {
-    #     PUID = "1000";
-    #     PGID = "1000";
-    #     TZ = "America/Chicago";
-    #   };
-    # };
+  config = {
+    cfg.docker.storage_root = "/Volumes/Server/docker/docker_storage_root";
 
-    # webserver = {
-    #   image = "linuxserver/letsencrypt";
-    #   ports = [ "80:80" "443:443" ];
-    #   volumes = [ "letsencrypt_config:/config" ];
-    #   environment = {
-    #     PUID = "1000";
-    #     PGID = "1000";
-    #     TZ = "America/Chicago";
-    #     URL = "cameron.computer";
-    #     SUBDOMAINS = "wildcard";
-    #     VALIDATION = "dns";
-    #     DNSPLUGIN = "cloudflare";
-    #     EMAIL = "csanders@protonmail.com";
-    #   };
+    # docker-containers = {
+    #   # portainer = {
+    #   #   image = "portainer/portainer";
+    #   #   ports = [ "8000:8000" "9000:9000" ];
+    #   #   volumes = [
+    #   #     "/var/run/docker.sock:/var/run/docker.sock"
+    #   #     "portainer_data:/data"
+    #   #   ];
+    #   #   environment = {
+    #   #     PUID = "1000";
+    #   #     PGID = "1000";
+    #   #     TZ = "America/Chicago";
+    #   #   };
+    #   # };
+
+    #   # webserver = {
+    #   #   image = "linuxserver/letsencrypt";
+    #   #   ports = [ "80:80" "443:443" ];
+    #   #   volumes = [ "letsencrypt_config:/config" ];
+    #   #   environment = {
+    #   #     PUID = "1000";
+    #   #     PGID = "1000";
+    #   #     TZ = "America/Chicago";
+    #   #     URL = "cameron.computer";
+    #   #     SUBDOMAINS = "wildcard";
+    #   #     VALIDATION = "dns";
+    #   #     DNSPLUGIN = "cloudflare";
+    #   #     EMAIL = "csanders@protonmail.com";
+    #   #   };
+    #   # };
     # };
   };
 }
