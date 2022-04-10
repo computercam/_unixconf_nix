ed97420300ada925fa75fcc36b54e08897804d35
Cameron Sanders <csanders@protonmail.com>
2022-04-10 16:59:45 -0500

    add nix on external drive

--- config/systems/nixos-homeserver/modules/filesystems.nix
+++ config/systems/nixos-homeserver/modules/filesystems.nix
@@ -1,21 +1,33 @@
 { config, lib, pkgs, options, ... }: {
-  config.fileSystems = {
-    Server = {
-      device = "/dev/disk/by-label/SERVER";
-      mountPoint = "/Volumes/Server";
-      fsType = "btrfs";
-    };
+  config = {
+    environment.systemPackages = with pkgs; [ btrfs-progs ];
 
-    Storage = {
-      device = "/dev/disk/by-label/STORAGE";
-      mountPoint = "/Volumes/Storage";
-      fsType = "ext4";
-    };
+    fileSystems = {
+      Server = {
+        device = "/dev/disk/by-label/SERVER";
+        mountPoint = "/Volumes/Server";
+        fsType = "btrfs";
+      };
 
-    Backup = {
-      device = "/dev/disk/by-label/BACKUP";
-      mountPoint = "/Volumes/Backup";
-      fsType = "ext4";
+      Storage = {
+        device = "/dev/disk/by-label/STORAGE";
+        mountPoint = "/Volumes/Storage";
+        fsType = "ext4";
+      };
+
+      Backup = {
+        device = "/dev/disk/by-label/BACKUP";
+        mountPoint = "/Volumes/Backup";
+        fsType = "ext4";
+      };
+
+      Nix = {
+        device = "/dev/disk/by-label/NIX";
+        mountPoint = "/nix";
+        fsType = "ext4";
+        neededForBoot = true;
+        options = [ "noatime" ];
+      };
     };
   };
 }
