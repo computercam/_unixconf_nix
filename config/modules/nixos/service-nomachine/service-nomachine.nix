22f344ce6a8444194b354532f87af83b9492bdfe
Cameron Sanders <csanders@protonmail.com>
2022-04-10 21:45:59 -0500

    nomachine screen lock

--- config/modules/nixos/service-nomachine/service-nomachine.nix
+++ config/modules/nixos/service-nomachine/service-nomachine.nix
@@ -7,8 +7,6 @@ with lib;
    ];
 
    config = mkMerge [({
-      # environment.systemPackages = with pkgs; [ nomachine ];
-      
       services.nxserver = {
          enable = true;
          enableDebug = true;
@@ -20,6 +18,15 @@ with lib;
       services.xserver.displayManager.gdm = {
          autoSuspend = mkForce false;
          wayland = mkForce false;
+      };
+   })
+   
+   (mkIf (config.services.xserver.xautolock.enable == true) {
+      # environment.systemPackages = with pkgs; [ xscreensaver ];
+
+      services.xserver.xautolock = {
+         time = 1;
+         locker = "${pkgs.xscreensaver}/bin/xscreensaver-command -lock";
       };
    })];
 }
