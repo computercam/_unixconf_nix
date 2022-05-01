9407f185bee93b2bfbede74bd3f710b73e10da4d
Cameron Sanders <csanders@protonmail.com>
2022-05-01 16:58:21 -0500

    development packages

--- config/systems/nixos-homeserver/modules/development.nix
+++ config/systems/nixos-homeserver/modules/development.nix
@@ -2,12 +2,12 @@
   config = {
     environment.systemPackages = with pkgs; [
         python3Full
-        python39Packages.pandas
-        python39Packages.numpy
-        python39Packages.seaborn
-        python39Packages.matplotlib
-        python39Packages.xgboost
-        python39Packages.ipykernel
+        python3Packages.pip
+        nodejs
+        nodePackages.npm
+        cargo
+        rustc
+        distrobox
     ];
   };
 }
