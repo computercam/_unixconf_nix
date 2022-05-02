21acd9655b29b44899e5bcaa27c6b56af669b3ec
Cameron Sanders <csanders@protonmail.com>
2022-05-01 20:36:10 -0500

    remove desktop environments

--- config/modules/nixos/desktop-environment-plasma/desktop-environment-plasma.nix
+++
@@ -1,5 +0,0 @@
-{ config, lib, pkgs, options, ... }:
-with lib; {
-  imports = [ ./modules.nix ];
-  config.services.xserver.desktopManager.plasma5.enable = true;
-}
--- config/modules/nixos/desktop-environment-xfce/desktop-environment-xfce.nix
+++
@@ -1,5 +0,0 @@
-{ config, lib, pkgs, options, ... }:
-with lib; {
-  imports = [ ./modules.nix ];
-  config.services.xserver.desktopManager.xfce.enable = true;
-}
