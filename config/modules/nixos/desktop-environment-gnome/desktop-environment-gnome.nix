a624d54c228382bcc1755e66cebbe76b0e952909
Cameron Sanders <csanders@protonmail.com>
2022-05-01 20:36:44 -0500

    remove extra packages

--- config/modules/nixos/desktop-environment-gnome/desktop-environment-gnome.nix
+++ config/modules/nixos/desktop-environment-gnome/desktop-environment-gnome.nix
@@ -2,15 +2,6 @@
   imports = [ ./modules.nix ];
 
   config = {
-    environment.systemPackages = with pkgs; [
-      gnome.gnome-terminal
-      gnome.gnome-tweaks
-      gnomeExtensions.appindicator
-      gnomeExtensions.dash-to-dock
-      gnomeExtensions.gsconnect
-      gnomeExtensions.just-perfection
-    ];
-
     programs.dconf.enable = true;
     services.gnome.gnome-keyring.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
