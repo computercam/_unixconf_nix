6a0ab4308f784c66d63bc0e20f2da8fffec11cbf
Cameron Sanders <csanders@protonmail.com>
2022-05-02 23:25:00 -0500

    add game drives

--- config/systems/nixos-homeserver/modules/filesystems.nix
+++ config/systems/nixos-homeserver/modules/filesystems.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, options, ... }: {
   config = {
-    environment.systemPackages = with pkgs; [ btrfs-progs ];
+    environment.systemPackages = with pkgs; [ btrfs-progs mergerfs ];
 
     fileSystems = {
       Server = {
@@ -27,6 +27,38 @@
         fsType = "ext4";
         neededForBoot = true;
         options = [ "noatime" ];
+      };
+
+      Game1 = {
+        device = "/dev/disk/by-label/GAME_1";
+        mountPoint = "/Volumes/.GAME/1";
+        fsType = "ext4";
+      };
+      Game2 = {
+        device = "/dev/disk/by-label/GAME_2";
+        mountPoint = "/Volumes/.GAME/2";
+        fsType = "ext4";
+      };
+      Game3 = {
+        device = "/dev/disk/by-label/GAME_3";
+        mountPoint = "/Volumes/.GAME/3";
+        fsType = "ext4";
+      };
+      Game4 = {
+        device = "/dev/disk/by-label/GAME_4";
+        mountPoint = "/Volumes/.GAME/4";
+        fsType = "ext4";
+      };
+      Game = {
+        device = "/Volumes/.GAME/*";
+        mountPoint = "/Volumes/Game";
+        fsType = "mergerfs";
+        options = [ 
+          "defaults" 
+          "allow_other" 
+          "use_ino" 
+          "fsname=Game"
+        ];
       };
     };
   };
