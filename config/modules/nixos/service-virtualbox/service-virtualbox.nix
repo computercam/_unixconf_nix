f46039f6511d1326402de66c456f3f819f36dd96
Cameron Sanders <csanders@protonmail.com>
2022-05-02 23:24:40 -0500

    fix virtualbox

--- config/modules/nixos/service-virtualbox/service-virtualbox.nix
+++ config/modules/nixos/service-virtualbox/service-virtualbox.nix
@@ -1,8 +1,6 @@
 { config, lib, pkgs, options, ... }: {
   config = {
-    environment.systemPackages = with pkgs; [ virtualbox ];
     users.users.main.extraGroups = [ "vboxusers" ];
-    boot.kernelModules = [ "vboxdrv" ];
     virtualisation.virtualbox.host.enable = true;
     virtualisation.virtualbox.host.enableExtensionPack = true;
   };
