c2536e3a9760b876fcb5401beb937f6519759725
Cameron <csanders@protonmail.com>
2022-05-11 19:32:53 -0500

    update utils

--- config/modules/common/utils/utils.nix
+++ config/modules/common/utils/utils.nix
@@ -3,28 +3,26 @@
     home-manager.users."${config.cfg.user.name}" = {
       home.packages = with pkgs; 
         if config.cfg.os.unix == "linux" then [
-          exfat # filesystems
-          ntfs3g # filesystems
           parted # filesystems
-          
           nettools # networking
           openvpn # networking
+          killall # processes
+          lshw # system info
         ] else if config.cfg.os.unix == "darwin" then [
         ] else [] ++
         [
-          uutils-coreutils # generic _ rust alt - coreutils # https://github.com/uutils/coreutils
-        
+          coreutils-full # generic
+
+          findutils # search
+          ripgrep # search _ rust alt - find + grep # https://github.com/BurntSushi/ripgrep
+
           bat # shell _ rust alt - cat & less # https://github.com/sharkdp/bat
           exa # shell _ rust alt - ls # https://github.com/ogham/exa
           zoxide # shell _ rust alt - cd # https://github.com/ajeetdsouza/zoxide
         
-          findutils # search
-          fzf # search
-          ripgrep # search _ rust alt - find + grep # https://github.com/BurntSushi/ripgrep
-        
           neofetch # system info
           lsof # system info
-          procs # system info _ rust alt - ps # https://github.com/dalance/procs
+          htop # system info _ rust alt - ps # https://github.com/dalance/procs
         
           nmap # networking
           speedtest-cli # networking
