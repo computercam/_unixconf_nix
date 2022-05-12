b7c1331fe4e02f0c054f326f2246f60ccc02e1d5
Cameron <csanders@protonmail.com>
2022-05-11 19:33:03 -0500

    update fonts

--- config/modules/common/fonts/fonts.nix
+++ config/modules/common/fonts/fonts.nix
@@ -1,12 +1,12 @@
 { config, lib, pkgs, options, ... }: {
   config.fonts = {
     fonts = with pkgs; [
-      # fantasque-sans-mono # included in nerdfonts
-      # fira-code # included in nerdfonts
-      # victor-mono # included in nerdfonts
+      # nerdfonts # huge package
+      fantasque-sans-mono # included in nerdfonts
+      fira-code # included in nerdfonts
+      victor-mono # included in nerdfonts
       corefonts
       dejavu_fonts
-      nerdfonts
       ubuntu_font_family
     ];
     fontDir.enable = true;
