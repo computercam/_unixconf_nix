fe1bc759d399ebdf5cc2c6db50e07d073816b309
Cameron <csanders@protonmail.com>
2022-05-11 19:34:43 -0500

    remove packages from system config

--- config/systems/darwin-workstation/configuration.nix
+++ config/systems/darwin-workstation/configuration.nix
@@ -9,52 +9,6 @@
     services.nix-daemon.enable = true;
     system.stateVersion = 4;
     nix.maxJobs = 10;
-
-    cfg.homebrew.casks = [        
-      "appcleaner"
-      "balenaetcher"
-      "bitwig-studio"
-      "blender"
-      "brave-browser"
-      "charles"
-      "chromium"
-      "cold-turkey-blocker"
-      "darktable"
-      "dash"
-      "disk-drill"
-      "docker"
-      "element"
-      "firefox"
-      "geekbench"
-      "gimp"
-      "grandperspective"
-      "handbrake"
-      "inkscape"
-      "iterm2"
-      "karabiner-elements"
-      "krita"
-      "launchcontrol"
-      "libreoffice"
-      "little-snitch"
-      "microsoft-teams"
-      "moom"
-      "musescore"
-      "notion"
-      "obs"
-      "obsidian"
-      "openvpn-connect"
-      "protonmail-bridge"
-      "protonvpn"
-      "resilio-sync"
-      "spotify"
-      "slack"
-      "the-unarchiver"
-      "transmission"
-      "visual-studio-code"
-      "vlc"
-      "wireshark"
-      "xquartz"
-      "zenmap"
-    ];
+    # cfg.yabai.enable = true;
   };
 }
