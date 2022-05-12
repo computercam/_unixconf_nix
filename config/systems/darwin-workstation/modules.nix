b3d145f8f5212756d5d5484df33580351d2ed231
Cameron <csanders@protonmail.com>
2022-05-11 19:34:50 -0500

    update modules

--- config/systems/darwin-workstation/modules.nix
+++ config/systems/darwin-workstation/modules.nix
@@ -1,11 +1,14 @@
 { config, pkgs, ... }: {
   imports = [
     ../../global/global.nix
+    ../../modules/common/dotfiles/dotfiles.nix
+    ../../modules/common/fonts/fonts.nix
     ../../modules/common/home-manager/home-manager.nix
     ../../modules/common/shell/shell.nix
     ../../modules/common/user-main/user-main.nix
     ../../modules/common/utils/utils.nix
     ../../modules/darwin/__darwin/__darwin.nix
-    ../../modules/darwin/application-homebrew/application-homebrew.nix
+    ../../modules/darwin/application-desktop/application-desktop.nix
+    # ../../modules/darwin/yabai/yabai.nix
   ];
 }
