674f4fa72500a6d8cfa1afece0869dc67e703cd2
cameron <csanders@protonmail.com>
2022-08-28 11:28:15 -0500

    adguard yaml

--- config/systems/nixos-netserver/modules/adguard.nix
+++ config/systems/nixos-netserver/modules/adguard.nix
@@ -1,29 +1,8 @@
-{ config, lib, pkgs, options, ... }:
-let
-  # https://github.com/paulyoung/pub2nix/blob/master/yaml.nix
-  fromYAML = yaml: builtins.fromJSON (
-    builtins.readFile (
-      pkgs.runCommand "from-yaml"
-        {
-          inherit yaml;
-          allowSubstitutes = false;
-          preferLocalBuild = true;
-        }
-        ''
-          ${pkgs.remarshal}/bin/remarshal  \
-            -if yaml \
-            -i <(echo "$yaml") \
-            -of json \
-            -o $out
-        ''
-    )
-  );
-  readYAML = path: fromYAML (builtins.readFile path);
-in {
+{ config, lib, pkgs, options, ... }: {
   config = {
     services.adguardhome = {
       enable = true;
-      settings = readYAML ./AdGuardHome.yaml;
+      settings = builtins.readFile ./AdGuardHome.yaml;
     };
 
     networking.firewall.allowedTCPPorts = [
