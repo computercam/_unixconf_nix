1e8b5250a351d9ad4791128646ead36c786e83e8
cameron <csanders@protonmail.com>
2022-08-28 11:06:57 -0500

    adguard settings

--- config/systems/nixos-netserver/modules.nix
+++ config/systems/nixos-netserver/modules.nix
@@ -6,14 +6,12 @@
 
     ../../modules/nixos/__nixos/__nixos.nix
     ../../modules/nixos/service-avahi/service-avahi.nix
-    ../../modules/nixos/service-docker/service-docker.nix
     ../../modules/nixos/service-firewall/service-firewall.nix
     ../../modules/nixos/service-networking/service-networking.nix
     ../../modules/nixos/service-ssh/service-ssh.nix
     ../../modules/nixos/service-sudo/service-sudo.nix
 
-    ./modules/docker-adguard.nix
-    ./modules/docker.nix
+    ./modules/adguard.nix
     ./modules/networking.nix
   ];
 }
--- config/systems/nixos-netserver/modules/docker.nix
+++
@@ -1,6 +0,0 @@
-{ config, lib, pkgs, options, ... }: {
-  config = {
-    cfg.docker.storage_root = "/home/cameron/docker/docker_storage_root";
-    cfg.docker.networking.iptables = "true";
-  };
-}
