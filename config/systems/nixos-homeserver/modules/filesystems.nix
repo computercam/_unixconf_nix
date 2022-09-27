2b5dfec5970076cff3a57e2f0ea1dd05bc19d2f9
cameron <csanders@protonmail.com>
2022-09-27 15:07:04 -0500

    storage and shares

--- config/systems/nixos-homeserver/modules/filesystems.nix
+++ config/systems/nixos-homeserver/modules/filesystems.nix
@@ -1,8 +1,23 @@
 { config, lib, pkgs, options, ... }: {
   config = {
-    environment.systemPackages = with pkgs; [ btrfs-progs mergerfs ];
+    environment.systemPackages = with pkgs; [ 
+      btrfs-progs 
+      mergerfs 
+    ];
 
     fileSystems = {
+      Backup = {
+        device = "/dev/disk/by-label/BACKUP";
+        mountPoint = "/Volumes/Backup";
+        fsType = "ext4";
+      };
+
+      BulkStorage = {
+        device = "/dev/disk/by-label/BULK_STORAGE";
+        mountPoint = "/Volumes/BulkStorage";
+        fsType = "ext4";
+      };
+
       Server = {
         device = "/dev/disk/by-uuid/76bc5abf-0185-41d0-8658-5038177def99";
         mountPoint = "/Volumes/Server";
@@ -16,10 +31,10 @@
         fsType = "ext4";
       };
 
-      Backup = {
-       device = "/dev/disk/by-label/BACKUP";
-       mountPoint = "/Volumes/Backup";
-       fsType = "ext4";
+      TimeMachine = {
+        device = "/dev/disk/by-label/TIME_MACHINE";
+        mountPoint = "/Volumes/TimeMachine";
+        fsType = "ext4";
       };
 
       # Game1 = {
