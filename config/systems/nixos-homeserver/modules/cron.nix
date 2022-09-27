3240ad3927cb7d5cc28b33cd4e0aad12268a6817
cameron <csanders@protonmail.com>
2022-09-27 15:07:14 -0500

    misc updates

--- config/systems/nixos-homeserver/modules/cron.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, pkgs, options, ... }: {
-  config.services.cron.systemCronJobs = [
-    "15   */6  *   *   *  root     rsync -avzh --min-size=1 /Volumes/Storage/ /Volumes/Backup/Storage/"
-    "15   */12 *   *   *  root     rsync -avzh --min-size=1 /Volumes/Server/docker/ /Volumes/Backup/Server/docker/"
-    "30   */4  *   *   *  cameron  rsync -avzh --min-size=1 /Volumes/Server/docker/gitea/git/repositories/ /Volumes/Storage/Cameron/Git/"
-    # "45   2    */7 *   *  root     cd /Volumes/Server/ && ./docker_snapshot.sh"
-  ];
-}
