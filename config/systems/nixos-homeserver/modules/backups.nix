91d9d43c66a13a2467ee0f77d9f567c14aeace0d
cameron <csanders@protonmail.com>
2022-10-05 14:50:45 -0500

    borg remote backups

--- config/systems/nixos-homeserver/modules/backups.nix
+++ config/systems/nixos-homeserver/modules/backups.nix
@@ -4,48 +4,76 @@ let
   BorgRootPath = "/Volumes/Backup/BorgBackup";
 
   defaults = {
-    encryption.mode = "none";
+    encryption.mode = "repokey";
+    encryption.passCommand = "cat ${config.age.secrets.borgbackup.path}";
     compression = "auto,lzma";
     persistentTimer = true;
-    startAt = "daily";
+    appendFailedSuffix = false;
+    exclude = [ "re:^.*\.sync/" ];
+    environment.BORG_RSH = ''ssh -i ${builtins.toString (./../../../../_ + "/id_rsa.borgbackup")}'';
   };
   
-  ServerLocal = mkMerge [ defaults {
+  Server = mkMerge [ defaults {
     paths = "/Volumes/Server";
     repo = "${BorgRootPath}/Server";
-    preHook = ''systemctl list-units --type=service --all \
-      | grep docker- \
-      | tr -s " " \
-      | cut -d" " -f 2  \
-      | xargs systemctl stop
+    preHook = ''
+      systemctl list-units --type=service --all \
+        | grep docker- \
+        | tr -s " " \
+        | cut -d" " -f 2  \
+        | xargs systemctl stop
     '';
-    postHook = ''systemctl list-units --type=service --all \
-      | grep docker- \
-      | tr -s " " \
-      | cut -d" " -f 2  \
-      | xargs systemctl restart
+    postHook = ''
+      systemctl list-units --type=service --all \
+        | grep docker- \
+        | tr -s " " \
+        | cut -d" " -f 2  \
+        | xargs systemctl restart
     '';
-    startAt = mkForce "*-*-* 02:00:00";
+    startAt = "*-*-* 01:00:00";
   }];
 
-  RaeLocal = mkMerge [ defaults {
+  ServerRemote = mkMerge [ Server {
+    repo = mkForce "ssh://u320463@u320463.your-storagebox.de:23/./Backup/BorgBackup/Server";
+    startAt = mkForce "*-*-* 01:30:00";
+  }];
+
+  Rae = mkMerge [ defaults {
     paths = "/Volumes/Storage/Rae";
     repo = "${BorgRootPath}/Rae";
+    startAt = "*-*-* 02:00:00";
+  }];
+
+  RaeRemote = mkMerge [ Rae {
+    repo = mkForce "ssh://u320463@u320463.your-storagebox.de:23/./Backup/BorgBackup/Rae";
     startAt = mkForce "*-*-* 02:30:00";
   }];
 
-  CameronLocal = mkMerge [ defaults {
+  Cameron = mkMerge [ defaults {
     paths = "/Volumes/Storage/Cameron";
     repo = "${BorgRootPath}/Cameron";
-    startAt = mkForce "*-*-* 03:00:00";
+    startAt = "*-*-* 03:00:00";
+  }];
+
+  CameronRemote = mkMerge [ Cameron {
+    repo = mkForce "ssh://u320463@u320463.your-storagebox.de:23/./Backup/BorgBackup/Cameron";
+    startAt = mkForce "*-*-* 03:30:00";
   }];
 
 in {
-  config.services.borgbackup = {
-    jobs = {
-      CameronLocal = CameronLocal;
-      RaeLocal = RaeLocal;
-      ServerLocal = ServerLocal;
+  config = {
+    age.identityPaths = [ ../../../../_/id_rsa.borgbackup ];
+    age.secrets.borgbackup.file = ../../../../secrets/borgbackup.age;
+
+    services.borgbackup = {
+      jobs = {
+        Cameron = Cameron;
+        CameronRemote = CameronRemote;
+        Rae = Rae;
+        RaeRemote = RaeRemote;
+        Server = Server;
+        ServerRemote = ServerRemote;
+      };
     };
   };
 }
