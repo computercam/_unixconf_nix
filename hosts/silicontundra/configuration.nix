a53b8f9aea43a99f402bdc50cca4197b21dd3135
Cameron Keathley <csanders@protonmail.com>
2023-11-22 14:39:18 -0600

    updating m1 pro config

--- hosts/silicontundra/configuration.nix
+++ hosts/silicontundra/configuration.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }: {
   imports = [ ./modules.nix ];
-  cfg.os.version = "23.05";
+  cfg.os.version = "23.11";
+  cfg.os.hostname = "silicontundra";
   nix.maxJobs = 10;
   nixpkgs.hostPlatform = "aarch64-darwin";
+  nix.useDaemon = true;
+
 }
--- hosts/silicontundra/modules.nix
+++ hosts/silicontundra/modules.nix
@@ -4,6 +4,7 @@
     ../../modules/common/fonts/fonts.nix
     ../../modules/common/home/home.nix
     # ../../modules/darwin/service-yabai/service-yabai.nix
-    # ../../modules/darwin/application-desktop/application-desktop.nix
+    ../../modules/darwin/application-desktop/application-desktop.nix
+    ../../modules/darwin/system-defaults/system-defaults.nix
   ];
 }
