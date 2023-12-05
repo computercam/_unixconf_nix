2659f808e70b5aa03f745435c04fc518c7353edb
Cameron Keathley <csanders@protonmail.com>
2023-12-05 12:45:49 -0600

    moving git modules around

--- hosts/silicontundra/configuration.nix
+++ hosts/silicontundra/configuration.nix
@@ -2,7 +2,7 @@
   imports = [ ./modules.nix ];
   cfg.os.version = "23.11";
   cfg.os.hostname = "silicontundra";
-  nix.maxJobs = 10;
+  nix.settings.max-jobs = 16;
   nixpkgs.hostPlatform = "aarch64-darwin";
   nix.useDaemon = true;
 
--- hosts/silicontundra/modules.nix
+++ hosts/silicontundra/modules.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }: {
   imports = [
-    #../../modules/common/home/dotfiles.nix 
+    ../../modules/common/home/dotfiles.nix 
     ../../modules/common/fonts/fonts.nix
     ../../modules/common/home/home.nix
     ../../modules/macos/application-desktop/application-desktop.nix
